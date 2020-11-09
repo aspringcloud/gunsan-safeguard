e8f990cad9681acd6a43ccde628c893d0f224ef5
hyeri <hrkang@aspringcloud.com>
2020-11-09 10:46:10 +0900

    :heavy_plus_sign: add alert in new call

--- src/views/operate.js
+++ src/views/operate.js
@@ -130,6 +130,7 @@ let operateMixin = {
         this.socketMsg.how.site_id == this.site.id
       ) {
         if (this.socketMsg.how.function == "call") {
+          alert("신규 배차 등록")
           this.convertCallInfo(this.socketMsg.how);
         } else if (this.socketMsg.how.function == "cancel_call") {
           console.log(this.calls[this.callUidChain[this.socketMsg.how.uid]]);
